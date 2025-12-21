52c4bcbbe3e68e5797c24b3677e8612047a4bb2e
python-lang-learner <python.learn.git@gmail.com>
2025-12-21 19:27:47 +0530

    Clean project structure and remove duplicates

--- js/layout.js
+++ js/layout.js
@@ -1,9 +1,20 @@
-function loadComponent(id, file) {
+/*function loadComponent(id, file) {
   fetch(file)
     .then(res => res.text())
     .then(data => {
       document.getElementById(id).innerHTML = data;
     });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadComponent("header", "/components/header.html");
+  loadComponent("footer", "/components/footer.html");
+});
+*/
+function loadComponent(id, file) {
+  fetch(file)
+    .then(res => res.text())
+    .then(html => document.getElementById(id).innerHTML = html);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
